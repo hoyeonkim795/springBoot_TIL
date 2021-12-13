c10a0b32c436494a5351d4ee1932b4c6c80b7c6b
hoyeonkim <hy940313@gmail.com>
2021-12-13 15:33:36 +0900

    initial commit

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // Hello-world (endpoint)
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello world";
+    }
+    // alt + enter
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello world");
+    }
+}
